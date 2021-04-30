ccb8257b51c6268ffe6804b81cf3cb7eeae80a8c
GenericKnowledge <karaaegean@gmail.com>
2021-04-30 21:13:32 +0300

    sign up ui and validators

--- app/src/main/java/school/cactus/succulentshop/LoginActivity.kt
+++ app/src/main/java/school/cactus/succulentshop/LoginActivity.kt
@@ -1,6 +1,8 @@
 package school.cactus.succulentshop
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.textfield.TextInputLayout
 import school.cactus.succulentshop.databinding.ActivityLoginBinding
@@ -23,7 +25,16 @@ class LoginActivity : AppCompatActivity() {
                 passwordInputLayout.validate()
                 identifierInputLayout.validate()
             }
+            createAccountButton.setOnClickListener {
+                navigateToSignUpActivity()
+            }
         }
+    }
+
+    private fun navigateToSignUpActivity() {
+        val intent = Intent(this,SignUpActivity::class.java)
+        startActivity(intent)
+
     }
 
     private fun TextInputLayout.validate() {
